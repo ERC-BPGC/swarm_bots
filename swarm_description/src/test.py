f0a2c5fe999b4151acb57224f64172ac4d2f8318
Dhruv Potdar <dhruvpotdar29@gmail.com>
2022-10-02 19:23:34 +0530

    changes

---
+++ swarm_description/src/test.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+from tf.transformations import euler_from_quaternion
+from nav_msgs.msg import Odometry
+from math import atan2, cos, sin, pi
+
+class PID:
+
+	def _init_(self,kp,ki,kd,final_coord,goals):
+		self.ed_dot, self.ed_old, self.Ed, self.ed, self.et_dot, self.et_old,self.Et,self.et, self.v, self.x, self.y, self.theta, self.w = 0,0,0,0,0,0,0,0,0,0,0,0,0
+		self.kp = kp
+		self.ki = ki
+		self.kd = kd
+		self.goals = goals
+		self.x_final, self.y_final = final_coord
+		self.theta_final = 0 #atan2(self.y_final,self.x_final)
+		self.motion = Twist()
+		self.odom_sub = rospy.Subscriber('bot0/odom', Odometry, self.bot_pos)
+		self.cmd_vel_pub = rospy.Publisher('bot0/cmd_vel', Twist, queue_size=1)
+		self.count = 0
+	
+	def bot_pos(self,msg):
+		self.x = msg.pose.pose.position.x
+		self.y = msg.pose.pose.position.y
+		rot_q = msg.pose.pose.orientation
+		(roll, pitch, theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
+		self.theta = theta #msg.pose.pose.orientation.z
+		self.theta_final = atan2(self.y_final-self.y, self.x_final-self.x)
+		self.vel_update()
+	
+	def reset(self):
+		self.ed_dot, self.ed_old, self.Ed, self.ed, self.et_dot, self.et_old,self.Et,self.et, self.v, self.x, self.y, self.theta, self.w = 0,0,0,0,0,0,0,0,0,0,0,0,0
+		self.theta_final = 0
+		self.count = 0
+		
+	def goal_change(self):
+		self.x_final, self.y_final = self.goals[0]
+		self.goals.pop(0)
+		
+	def distance(self,p1,p2):
+		x1,y1 = p1
+		x2,y2 = p2
+		return ((x1-x2)**2 + (y1-y2)**2)*0.5
+	
+	def vel_update(self):
+		self.ed  = self.distance((self.x_final, self.y_final),(self.x,self.y))
+		self.et = self.theta_final - self.theta
+		self.et = atan2(sin(self.et),cos(self.et))
+
+		if self.ed <= 0.1:
+			self.motion.linear.x = 0
+			self.motion.linear.y = 0
+			self.motion.angular.z = 0
+			self.cmd_vel_pub.publish(self.motion)
+			if len(self.goals) > 0:
+				self.reset()
+				self.goal_change()
+			
+		else:
+			if self.count == 0:
+				self.ed_dot = 0
+				self.et_dot = 0
+			else:
+				self.ed_dot = self.ed - self.ed_old
+				self.et_dot = self.et - self.et_old
+				
+			self.Ed += self.ed
+			self.Et += self.et
+			self.v = self.kp*self.ed + self.kd*self.ed_dot + self.ki*self.Ed
+			self.w = self.kp*self.et + 0*self.kd*self.et_dot + self.ki*self.Et
+			self.ed_old = self.ed
+			self.et_old = self.et
+			if self.v > 0:
+				self.motion.linear.x = min(self.v, 0.5)
+			else:
+				self.motion.linear.x = max(self.v, -0.5)
+			if self.w > 1.5:
+				self.w = 1.5
+			if self.w < -1.5:
+				self.w = -1.5
+			self.motion.angular.z = self.w
+			
+			if abs(self.et) > 0.1:
+				self.motion.linear.x = 0
+			else:
+				self.motion.angular.z = 0
+			
+			self.cmd_vel_pub.publish(self.motion)
+			print(self.v)
+			self.count+=1
+
+rospy.init_node('pid_controller')	
+
+if __name__ == '_main_':
+	PID(0.75, 0,0.5,(-1,0),[(-2,0),(0,0),(0,1)])
+	rospy.spin()
---
+++ swarm_perception/src/apriltag_detection.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+import rospy
+from apriltag import apriltag
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+
+ros_img = Image()
+
+def img_callback(msg):
+	global ros_img
+
+	rospy.loginfo("Image Received")
+	ros_img = msg
+	cv_img = bridge.imgmsg_to_cv2(ros_img, "bgr8")
+	
+	gray = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
+	detector = apriltag("tag36h11")
+	results = detector.detect(gray)
+
+	for i in range(0,len(results)):
+		# Corners of the tag
+		lb = int(results[i]['lb-rb-rt-lt'][0][0]),int(results[i]['lb-rb-rt-lt'][0][1])
+		rb = int(results[i]['lb-rb-rt-lt'][1][0]),int(results[i]['lb-rb-rt-lt'][1][1])
+		rt = int(results[i]['lb-rb-rt-lt'][2][0]),int(results[i]['lb-rb-rt-lt'][2][1])
+		lt = int(results[i]['lb-rb-rt-lt'][3][0]),int(results[i]['lb-rb-rt-lt'][3][1])
+		center = int(results[i]['center'][0]),int(results[i]['center'][1])
+		
+		top_center = int((lt[0]+rt[0])/2),int((lt[1]+rt[1])/2)
+		# Draw the bounding boxes
+		rospy.loginfo("AprilTag Detected, Drawing Bounding Boxes")
+		cv2.line(cv_img,lb,rb,(0,255,0),6)
+		cv2.line(cv_img,rb,rt,(0,255,0),6)
+		cv2.line(cv_img,rt,lt,(0,255,0),6)
+		cv2.line(cv_img,lt,lb,(0,255,0),6)
+
+		# Draw arrow from center of tag to center of top edge
+		cv2.arrowedLine(cv_img,center,top_center, (0,0,255),6,tipLength=0.1, line_type=cv2.LINE_AA)
+
+		# Draw Dots at the corners
+		cv2.circle(cv_img,lb,10,(255, 255, 0),-1)
+		cv2.circle(cv_img,rb,10,(255, 0, 255),-1)
+		cv2.circle(cv_img,rt,10,(0, 225, 255),-1)
+		cv2.circle(cv_img,lt,10,(255, 0, 0),-1)
+		rate= rospy.Rate(5)
+		rate.sleep()
+
+
+	rospy.loginfo("Publishing Image")
+	img_pub.publish(bridge.cv2_to_imgmsg(cv_img, "bgr8"))
+	# cv2.imshow('Tags',cv_img)
+	# cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+	bridge = CvBridge()
+	rospy.init_node('apriltag_detection')
+	rospy.loginfo("AprilTag Node Started")
+	sub = rospy.Subscriber('/camera/rgb/image_raw', Image, img_callback)
+	img_pub = rospy.Publisher('/detection_image', Image, queue_size=10) 
+	rospy.spin()
+
+
+
